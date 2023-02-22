d07283b316237faabd928a97329f48ee73e600a9
Tárcio T <108308831+teixtarcio@users.noreply.github.com>
2023-02-22 16:35:59 -0300

    Implementando funções para leitura e display de arquivos CSV

--- src/main/java/com/example/application/views/csv/CSVView.java
+++ src/main/java/com/example/application/views/csv/CSVView.java
@@ -1,36 +1,69 @@
 package com.example.application.views.csv;
 
 import com.example.application.views.MainLayout;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.shared.util.SharedUtil;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @PageTitle("CSV")
 @Route(value = "csv", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
 public class CSVView extends VerticalLayout {
+    Grid<String[]> grid = new Grid<>();
 
     public CSVView() {
-        setSpacing(false);
+        var importButton = new Button("Import from classpath", e -> readFromClasspath());
+        var buffer = new MemoryBuffer();
+        var upload = new Upload(buffer);
+        upload.addSucceededListener(e -> {
+            displayCsv(buffer.getInputStream());
+        });
+        add(grid, new HorizontalLayout(importButton, upload)
+        );
+    }
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+    private void readFromClasspath(){
+        displayCsv(getClass().getClassLoader().getResourceAsStream("2023_LoL_esports_match_data_from_OraclesElixir.csv"));
+    }
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+    private void displayCsv(InputStream resourceAsStream) {
+        var parser = new CSVParserBuilder().withSeparator(';').build();
+        var reader = new CSVReaderBuilder(new InputStreamReader(resourceAsStream)).withCSVParser(parser).build();
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        try {
+            var entries = reader.readAll();
+
+            var headers = entries.get(0);
+
+            for (int i = 0; i < headers.length; i++) {
+                int colIndex = i;
+                grid.addColumn(row -> row[colIndex])
+                        .setHeader(SharedUtil.camelCaseToHumanFriendly(headers[colIndex]));
+            }
+
+            grid.setItems(entries.subList(1, entries.size()));
+
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
     }
 
 }
